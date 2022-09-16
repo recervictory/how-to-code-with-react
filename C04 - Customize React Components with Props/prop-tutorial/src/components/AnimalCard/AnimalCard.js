a10b80c1f7de7ed4578929c855ca78bab39bd2da
Victor Banerjee <recervictory@gmail.com>
2022-09-16 14:42:06 +0530

    Step 2 — Building Dynamic Components with Props

---
+++ C04 - Customize React Components with Props/prop-tutorial/src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './AnimalCard.css';
+
+export default function AnimalCard({
+  additional,
+  diet,
+  name,
+  scientificName,
+  size,
+  showAdditional,
+}) {
+  return (
+    <div className='animal-wrapper'>
+      <h2>{name}</h2>
+      <h3>{scientificName}</h3>
+      <h4>{size}kg</h4>
+      <div>{diet.join(', ')}.</div>
+      <button onClick={() => showAdditional(additional)}> More Info </button>
+    </div>
+  );
+}
--- C04 - Customize React Components with Props/prop-tutorial/src/components/App/App.js
+++ C04 - Customize React Components with Props/prop-tutorial/src/components/App/App.js
@@ -1,9 +1,30 @@
+import data from "./data";
+import "./App.css";
+import AnimalCard from "../AnimalCard/AnimalCard";
 
-import './App.css';
+function showAdditional(additional) {
+  const alertInformation = Object.entries(additional)
+    .map(information => `${information[0]}: ${information[1]}`)
+    .join('\n');
+  alert(alertInformation)
+};
 
 function App() {
   return (
-    <></>
+    <div className="wrapper">
+      <h1>Animals</h1>
+      {data.map((animal) => (
+        <AnimalCard
+          key={animal.name}
+          name={animal.name}
+          additional={animal.additional}
+          diet={animal.diet}
+          scientificName={animal.scientificName}
+          size={animal.size}
+          showAdditional={showAdditional}
+        />
+      ))}
+    </div>
   );
 }
 
