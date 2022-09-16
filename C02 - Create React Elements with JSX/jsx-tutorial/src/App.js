09b308fb2d6492fa9f3671984b4f07abda0c9639
Victor Banerjee <recervictory@gmail.com>
2022-09-16 11:15:44 +0530

    Step 3 — Adding Events to Elements

--- C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
+++ C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
@@ -3,27 +3,28 @@ import "./App.css";
 
 function App() {
   const greeting = "greeting";
+  const displayEmojiName = (event) => alert(event.target.id);
   return (
     <div className="container">
       <h1 id={greeting}>Hello, World</h1>
       <p>I am writing jsx</p>
       <ul>
         <li>
-          <button>
+          <button onClick={displayEmojiName}>
             <span role="img" aria-label="grinning face" id="grinning face">
               😀
             </span>
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={displayEmojiName}>
             <span role="img" aria-label="party popper" id="party popper">
               🎉
             </span>
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={displayEmojiName}>
             <span role="img" aria-label="woman dancing" id="woman dancing">
               💃
             </span>
