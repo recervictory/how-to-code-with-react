4ee1b6cd08d4fcfa85ae4cc48bb2deeae0b9de60
Victor Banerjee <recervictory@gmail.com>
2022-09-16 11:24:05 +0530

    Step 3 — Adding Events to Elements

--- C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
+++ C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
@@ -3,33 +3,37 @@ import "./App.css";
 
 function App() {
   const greeting = "greeting";
+
+  const emojis = [
+    {
+      emoji: "😀",
+      name: "grinning face",
+    },
+    {
+      emoji: "🎉",
+      name: "party popper",
+    },
+    {
+      emoji: "💃",
+      name: "woman dancing",
+    },
+  ];
+
   const displayEmojiName = (event) => alert(event.target.id);
   return (
     <div className="container">
       <h1 id={greeting}>Hello, World</h1>
       <p>I am writing jsx</p>
       <ul>
-        <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="grinning face" id="grinning face">
-              😀
-            </span>
-          </button>
-        </li>
-        <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="party popper" id="party popper">
-              🎉
-            </span>
-          </button>
-        </li>
-        <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="woman dancing" id="woman dancing">
-              💃
-            </span>
-          </button>
-        </li>
+        {emojis.map((emoji) => (
+          <li key={emoji.name}>
+            <button onClick={displayEmojiName}>
+              <span role="img" aria-label={emoji.name} id={emoji.name}>
+                {emoji.emoji}
+              </span>
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
