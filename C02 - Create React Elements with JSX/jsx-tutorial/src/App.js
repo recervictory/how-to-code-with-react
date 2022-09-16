97215fdfd6843743b58fcad146f16f8644342e14
Victor Banerjee <recervictory@gmail.com>
2022-09-16 10:40:35 +0530

    Step 2  -- Adding Styling with Attributes

--- C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
+++ C02 - Create React Elements with JSX/jsx-tutorial/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const greeting = "greeting";
   return (
-    <>
-    <h1>Hello, World</h1>
-    <p>I am writing jsx</p>
-    </>
-  )
-  ;
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
+      <p>I am writing jsx</p>
+      <ul>
+        <li>
+          <button>
+            <span role="img" aria-label="grinning face" id="grinning face">
+              😀
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="party popper" id="party popper">
+              🎉
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="woman dancing" id="woman dancing">
+              💃
+            </span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
