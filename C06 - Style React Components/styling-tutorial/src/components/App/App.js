38509d9a94b2efd07e19e6a17592e97bf320b9b9
Victor Banerjee <recervictory@gmail.com>
2022-09-17 12:11:49 +0530

    Step 4 — Styling with JSS

--- C06 - Style React Components/styling-tutorial/src/components/App/App.js
+++ C06 - Style React Components/styling-tutorial/src/components/App/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
+import React from "react";
+import { createUseStyles } from "react-jss";
+
 import Alert from "../Alert/Alert";
 import CartSuccess from "../CartSuccess/CartSuccess";
 
+const useStyles = createUseStyles({
+  wrapper: {
+    padding: 20,
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="wrapper">
+    <div className={classes.wrapper}>
       <Alert title="Items Not Added" type="error">
         <div>Your items are out of stock</div>
       </Alert>
-      <CartSuccess/>
+      <CartSuccess />
     </div>
   );
 }
--- C06 - Style React Components/styling-tutorial/src/components/CartSuccess/CartSuccess.js
+++ C06 - Style React Components/styling-tutorial/src/components/CartSuccess/CartSuccess.js
@@ -1,17 +1,39 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+
 import Alert from "../Alert/Alert";
-import "./CartSuccess.css";
+
+const useStyles = createUseStyles({
+  item: {
+    marginRight: 20,
+  },
+  wrapper: {
+    borderTop: "black solid 1px gray",
+    display: "flex",
+    flexWrap: "wrap",
+    "& h2": {
+      //  same as .wrapper h2 i.e Inside wrapper class h2 tag
+      width: "100%",
+    },
+    "& item": {
+      marginRight: 20,
+    },
+  },
+});
 
 export default function CartSuccess() {
+  const classes = useStyles();
+
   return (
     <Alert title="Added to Cart" type="success">
-      <div className="cart-success-wrapper">
+      <hr />
+      <div className={classes.wrapper}>
         <h2>You have added 3 items:</h2>
-        <div className="item">
+        <div className={classes.item}>
           <div>Bananas</div>
           <div>Quantity: 2</div>
         </div>
-        <div className="item">
+        <div className={classes.item}>
           <div>Lettuce</div>
           <div>Quantity: 1</div>
         </div>
