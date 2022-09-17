dcfd94265f6370c82938666500c906e5e86785c7
Victor Banerjee <recervictory@gmail.com>
2022-09-17 11:33:40 +0530

    Step 2 — Styling a Component with Plain CSS

--- C06 - Style React Components/ReadMe.md
+++ C06 - Style React Components/ReadMe.md
@@ -1,1 +1,2 @@
 [How To Style React Components](https://www.digitalocean.com/community/tutorials/how-to-style-react-components)
+
---
+++ C06 - Style React Components/styling-tutorial/src/components/Alert/Alert.js
@@ -0,0 +1,24 @@
+import React from  "react";
+import PropTypes from "prop-types";
+
+import './Alert.css';
+
+
+export default function Alert({ children, title, type}) {
+  return (
+    <div className={`alert-wrapper ${type}`}>
+    <h2>{title}</h2>
+    {children}
+    </div>
+  )
+}
+
+
+Alert.prototype = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.element),
+        PropTypes.element.isRequired
+    ]),
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+}
