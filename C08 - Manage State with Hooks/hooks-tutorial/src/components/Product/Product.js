d292d5ef5b214356ccc1665c2d6fcd6f80f9d5df
Victor Banerjee <recervictory@gmail.com>
2022-09-17 16:47:16 +0530

    Step 3A — Using useState Method update State

--- C08 - Manage State with Hooks/hooks-tutorial/src/components/Product/Product.js
+++ C08 - Manage State with Hooks/hooks-tutorial/src/components/Product/Product.js
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import "./Product.css";
 
+const products = [
+  {
+    emoji: "🍦",
+    name: "ice cream",
+    price: 5,
+  },
+  {
+    emoji: "🍩",
+    name: "donuts",
+    price: 2.5,
+  },
+  {
+    emoji: "🍉",
+    name: "watermelon",
+    price: 4,
+  },
+];
+
 const currencyOptions = {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
@@ -12,21 +30,31 @@ function getTotal(total) {
 }
 
 export default function Product() {
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
 
-  
+  function add(product) {
+    setCart((current) => [...current, product.name]);
+    setTotal((current) => current + product.price);
+  }
 
   return (
     <div className="wrapper">
       <div>Shopping Cart: {cart.length} total items.</div>
       <div>Total: {getTotal(total)}</div>
-      <div className="product">
-        <span role="img" aria-label="ice cream">
-          🍦
-        </span>
+      <div>
+        {products.map((product) => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>
+                {product.emoji}
+              </span>
+            </div>
+            <button onClick={() => add(product)}>Add</button>{" "}
+            <button>Remove</button>
+          </div>
+        ))}
       </div>
-      <button>Add</button> <button>Remove</button>
     </div>
   );
 }
