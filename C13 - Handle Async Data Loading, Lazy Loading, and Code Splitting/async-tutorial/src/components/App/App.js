75313859c7d1704186afd3ea6ecd3abf4e92f393
Victor Banerjee <recervictory@gmail.com>
2022-09-20 14:14:03 +0530

    Step 1 — Loading Asynchronous Data with useEffect

--- C13 - Handle Async Data Loading, Lazy Loading, and Code Splitting/async-tutorial/src/components/App/App.js
+++ C13 - Handle Async Data Loading, Lazy Loading, and Code Splitting/async-tutorial/src/components/App/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import RiverInformation from '../RiverInformation/RiverInformation';
+import React, { useState } from "react";
+import RiverInformation from "../RiverInformation/RiverInformation";
 import "./App.css";
 
-
 function App() {
-  return <div className="wrapper">
-  <h1>World's Longest River</h1>
-  <RiverInformation/>
-  </div>;
+  const [river, setRiver] = useState("nile");
+
+  return (
+    <div className="wrapper">
+      <h1>World's Longest River</h1>
+      <button onClick={() => setRiver("nile")}>Nile</button>
+      <button onClick={() => setRiver("amazon")}>Amazon</button>
+      <button onClick={() => setRiver("yangtze")}>Yangtze</button>
+      <button onClick={() => setRiver("mississippi")}>Mississippi</button>
+      <RiverInformation name={river} />
+    </div>
+  );
 }
 
 export default App;
