f8798e4bbd38acea7012a3533c11ce75e662c64e
Victor Banerjee <recervictory@gmail.com>
2022-09-23 17:35:42 +0530

    Step 1 — Setting Up a Store

--- C15 - Manage State in React with Redux/redux-tutorial/src/components/App/App.js
+++ C15 - Manage State in React with Redux/redux-tutorial/src/components/App/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const birds = useSelector((state) => state.birds);
   return (
-    <></>
+    <div className="wrapper">
+      <h1>Bird List</h1>
+      <form>
+        <label>
+          <p>Add Bird</p>
+          <input type="text" />
+        </label>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <ul>
+        {birds.map((bird) => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>
+              Views: {bird.views}
+              <button>
+                <span role="img" aria-label="add">
+                  ➕
+                </span>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- C15 - Manage State in React with Redux/redux-tutorial/src/index.js
+++ C15 - Manage State in React with Redux/redux-tutorial/src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Todo: Redux component
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+// Todo:  Redux Store
+// The store is your central collection of data. In the next step, you’ll learn to create reducers that will set the default values and update your store, but for now you will hard-code the data.
+
+const store = createStore ( () => ({
+  birds: [
+    {
+      name: 'robin',
+      views: 1
+    }
+  ]
+})) 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
