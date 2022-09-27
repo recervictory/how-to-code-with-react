b91bea0dd0b0fcb25069d64cb11b65dc80600d22
Victor Banerjee <recervictory@gmail.com>
2022-09-27 11:33:45 +0530

    Step 1 — Installing React Router

--- C16 - Handle Routing in React/router-tutorial/src/components/App/App.js
+++ C16 - Handle Routing in React/router-tutorial/src/components/App/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import Manatee from "../Manatee/Manatee";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return (<div className="wrapper">
+  <h1>Marine Mammals</h1>
+  <Manatee/>
+  
+  </div>);
 }
 
 export default App;
