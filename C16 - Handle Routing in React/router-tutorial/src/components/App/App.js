c394fc757b057d518cce4f4a0474a708c17d806b
Victor Banerjee <recervictory@gmail.com>
2022-09-27 11:40:55 +0530

    Step 2 — Adding Routes

--- C16 - Handle Routing in React/router-tutorial/src/components/App/App.js
+++ C16 - Handle Routing in React/router-tutorial/src/components/App/App.js
@@ -1,12 +1,43 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Manatee from "../Manatee/Manatee";
+import Narwhal from "../Narwhal/Narwhal";
+import Whale from "../Whale/Whale";
+
 import "./App.css";
 
 function App() {
-  return (<div className="wrapper">
-  <h1>Marine Mammals</h1>
-  <Manatee/>
-  
-  </div>);
+  return (
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/manatee">Manatee</Link>
+            </li>
+            <li>
+              <Link to="/narwhal">Narwhal</Link>
+            </li>
+            <li>
+              <Link to="/whale">Whale</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/manatee">
+            <Manatee />
+          </Route>
+          <Route path="/narwhal">
+            <Narwhal />
+          </Route>
+          <Route path="/whale">
+            <Whale />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
